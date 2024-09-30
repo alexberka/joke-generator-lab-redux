c4d1d2c7d94e65199d65e07fe44363c01d4afce1
Alex Berka <alexanderberka@gmail.com>
2024-09-30 15:08:27 -0500

    Tic tac toe, no AI (random)

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import Board from '../components/Board';
+
 function Home() {
+  const [status, setStatus] = useState('playing');
+  const [gameCount, setGameCount] = useState(1);
+  const [record, setRecord] = useState({ wins: 0, losses: 0, ties: 0 });
+
+  const resetGame = () => {
+    setStatus('playing');
+    setGameCount((prev) => prev + 1);
+  };
+
+  const endGame = (winner) => {
+    if (winner === 'player') {
+      setRecord((prev) => ({ ...prev, wins: prev.wins + 1 }));
+    } else if (winner === 'computer') {
+      setRecord((prev) => ({ ...prev, losses: prev.losses + 1 }));
+    } else if (winner === 'tie') {
+      setRecord((prev) => ({ ...prev, ties: prev.ties + 1 }));
+    }
+    setStatus('ended');
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      Welcome to Next JS!
-    </div>
+    <>
+      <h1 className="header">Tic Tac Toe</h1>
+      <Board key={gameCount} onUpdate={(winner) => endGame(winner)} />
+      <div className="extras">
+        <h5 className="record">
+          Current Record: {record.wins} - {record.losses} - {record.ties}
+        </h5>
+        {status === 'ended' && (
+          <button type="button" className="new-game" onClick={resetGame}>
+            Play another game?
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
